fe41c4807df64a1c1e176af29ea0bbe27348f6aa
gajendrapise <97894108+gajendrapise@users.noreply.github.com>
2022-02-19 13:04:20 +0530

    here session start

---
+++ myclinic/co.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+header("location:contact.php");
+include 'conn.php';
+$fname=$_POST["fname"];
+  $lname=$_POST["lname"];
+  $email=$_POST["email"];
+  $message=$_POST["message"];
+$q="SELECT * FROM contactus WHERE firstname='$fname' && lastname='$lname' && email='$email' && message='$message'";
+$query=mysqli_query($con,$q);
+$num=mysqli_num_rows($query);
+if ($num==1) {
+	echo "duplicate data";
+} else {
+	$qy="INSERT INTO contactus (`firstname`, `lastname`, `email`, `message`) VALUES('".$fname."','".$lname."','".$email."','".$message."')";
+	mysqli_query($con,$qy);
+}
+?>
