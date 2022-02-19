bb966a38af96b591fc777d10406ee317975e4bf7
gajendrapise <97894108+gajendrapise@users.noreply.github.com>
2022-02-19 13:08:41 +0530

    here sessions start

--- myclinic/appointment.php
+++ myclinic/appointment.php
@@ -1,18 +1,7 @@
-<?php
-include 'conn.php';
-if (isset($_POST["done"])) {
-  $fname=$_POST["fname"];
-  $lname=$_POST["lname"];
-  $email=$_POST["email"];
-  $message=$_POST["message"];
-  $q="INSERT INTO contactus (`firstname`, `lastname`, `email`, `message`) VALUES('".$fname."','".$lname."','".$email."','".$message."')";
-  $query=mysqli_query($con,$q);
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap 5 Website Example</title>
+  <title>Appointment Management System</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
